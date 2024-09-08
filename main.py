736a3fd96ae23098c4e1db290bd6ecdc17953fa7
Ajay <88961945+AjayK47@users.noreply.github.com>
2024-09-08 12:14:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import autogen
 config_list = autogen.config_list_from_json(
     "OAI_CONFIG_LIST.json",
     filter_dict={
-        "model": ["gpt-4o-mini"],
+        "model": ["llama-3.1-70b-versatile"], # gpt-4o-mini
     },
 )
 
